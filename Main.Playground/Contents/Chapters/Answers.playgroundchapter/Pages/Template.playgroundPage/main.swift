e2a9e1b0ae706e64ad7adda8bb3b337ff225000c
Adam Fraser <41173209+solopaw@users.noreply.github.com>
2021-05-30 17:34:20 +1000

    Create main.swift

---
+++ Main.Playground/Contents/Chapters/Answers.playgroundchapter/Pages/Template.playgroundPage/main.swift
@@ -0,0 +1,11 @@
+//#-hidden-code
+import UIKit
+import PlaygroundSupport
+//#-end-hidden-code
+//#-editable-code Tap to enter code
+class ViewController:UIViewController{
+}
+//#-end-editable-code
+//#-hidden-code
+PlaygroundPage.current.setLiveView(ViewController())
+//#-end-hidden-code
